ca131a816333baa4d9819e46b5da76a61f78602d
DeveLooperUK <57773219+DeveLooperUK@users.noreply.github.com>
2022-05-13 08:05:15 +0100

    WishlistCartStepDefinitions Class implemented as a template

--- src/test/java/com/automation/step_defs/WishlistCartStepDefinitions.java
+++ src/test/java/com/automation/step_defs/WishlistCartStepDefinitions.java
@@ -1,4 +1,45 @@
 package com.automation.step_defs;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class WishlistCartStepDefinitions {
+
+    @Given("I go to website")
+    public void i_go_to_website() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("I add four different products to my wishlist")
+    public void i_add_four_different_products_to_my_wishlist() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I view my wishlist table")
+    public void i_view_my_wishlist_table() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("I find total four selected items in my wishlist")
+    public void i_find_total_four_selected_items_in_my_wishlist() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("I search for the lowest price item")
+    public void i_search_for_the_lowest_price_item() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I add the lowest price item to my cart")
+    public void i_add_the_lowest_price_item_to_my_cart() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("I verify the item in my cart")
+    public void i_verify_the_item_in_my_cart() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
