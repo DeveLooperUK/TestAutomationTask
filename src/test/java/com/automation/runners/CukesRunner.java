523aa02f49f6df662e06a4317d286ed035108c7c
DeveLooperUK <57773219+DeveLooperUK@users.noreply.github.com>
2022-05-13 08:02:43 +0100

    Runner Classes implemented

--- src/test/java/com/automation/runners/CukesRunner.java
+++ src/test/java/com/automation/runners/CukesRunner.java
@@ -1,4 +1,18 @@
 package com.automation.runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json",
+                "html:target/default-html-reports",
+                "rerun:target/rerun.txt"},
+        features = "src/test/resources/features",
+        glue = "com/automation/step_defs",
+        dryRun = true,
+        tags = ""
+)
 public class CukesRunner {
 }
--- src/test/java/com/automation/runners/FailedTestRunner.java
+++ src/test/java/com/automation/runners/FailedTestRunner.java
@@ -1,4 +1,14 @@
 package com.automation.runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"html:target/failed-html-report"},
+        features = "@target/rerun.txt",
+        glue = "com/automation/step_defs"
+)
 public class FailedTestRunner {
 }
