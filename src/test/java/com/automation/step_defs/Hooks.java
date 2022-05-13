3779e8a5fb290dc1451c593c53102a2d4f5d04eb
DeveLooperUK <57773219+DeveLooperUK@users.noreply.github.com>
2022-05-13 08:00:26 +0100

    Driver and Hooks implemented

--- src/test/java/com/automation/step_defs/Hooks.java
+++ src/test/java/com/automation/step_defs/Hooks.java
@@ -1,4 +1,30 @@
 package com.automation.step_defs;
 
+import com.automation.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.util.concurrent.TimeUnit;
+
 public class Hooks {
+
+    @Before
+    public void setUp(){
+        System.out.println("\tthis is coming from BEFORE");
+        Driver.get().manage().window().maximize();
+        Driver.get().manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void tearDown(Scenario scenario){
+        if(scenario.isFailed()){
+            final byte[] screenshot = ((TakesScreenshot) Driver.get()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png","screenshot");
+        }
+
+        Driver.closeDriver();
+    }
 }
